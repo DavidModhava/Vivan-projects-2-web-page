c3d2d231c14da1854edfc991675594ff7a82cfaf
DavidModhava <154404531+DavidModhava@users.noreply.github.com>
2023-12-31 18:31:59 +0530

    Add files via upload

---
+++ VIVAN TASK AND PRACTICE FILES/VIVON PROJECT TASK-1.js
@@ -0,0 +1,8 @@
+
+$(document).ready(function(){
+  $("#bt1").click(function(){
+      // Check if the device width is less than or equal to 768px (tablet/mobile view//
+          $(".tab").toggle(1000);
+    
+  });
+});
